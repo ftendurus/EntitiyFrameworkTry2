1169291458a44084a3a93afef0947e9c25bd3158
Furkan <ftendurus@outlook.com>
2022-08-05 12:12:22 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Business;
+using DataAccess;
+using System;
+
+namespace EntitiyFrameworkTry2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+            foreach (var customer in customerManager.GetAll())
+            {
+                Console.WriteLine("{0} / {1}",customer.CustomerID,customer.CompanyName);
+            }
+        }
+    }
+}
